c867ab2846abdc04662144bfcfcacc167162af9e
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2023-06-10 15:57:31 +0800

    some  modified about global.h and global.c

--- include/global.h
+++ include/global.h
@@ -2,30 +2,29 @@
 #define GLOBAL_H
 #undef __STRICT_ANSI__
 
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_audio.h>
-#include <allegro5/allegro_acodec.h>
+#include "allegro5/allegro.h"
+#include "allegro5/allegro_primitives.h"
+#include "allegro5/allegro_font.h"
+#include "allegro5/allegro_ttf.h"
+#include "allegro5/allegro_image.h"
+#include "allegro5/allegro_audio.h"
+#include "allegro5/allegro_acodec.h"
 #include <stdbool.h>
 
 // Debug
-#define Debug 1
-#define ShowMap 0
+#define Debug 0
 
 // Color
-#define COLOR_NUM 8
+#define COLOR_NUM 9
 #define BLACK  al_map_rgb(  0,  0,  0)
+#define GRAY   al_map_rgb(128,128,128)
+#define WRITE  al_map_rgb(255,255,255)
 #define RED    al_map_rgb(255,  0,  0)
 #define GREEN  al_map_rgb(  0,255,  0)
 #define BLUE   al_map_rgb(  0,  0,255)
-#define YELLOW al_map_rgb(255,255,  0)
 #define CMY    al_map_rgb(  0,255,255)
 #define PINK   al_map_rgb(255,  0,255)
-#define WRITE  al_map_rgb(255,255,255)
-#define GRAY   al_map_rgb(128,128,128)
+#define YELLOW al_map_rgb(255,255,  0)
 
 // Other
 #define MAX_PATH_LEN 1024
--- src/global.c
+++ src/global.c
@@ -4,6 +4,6 @@
 #include <wchar.h>
 
 // Error
-inline void raise_err(const char* msg) {if (Debug) fwprintf(stderr,L"error: %s\n", msg);}
+inline void raise_err(const char* msg) {fwprintf(stderr,L"error: %s\n", msg);}
 inline void raise_warn(const char* msg){if (Debug) fwprintf(stderr,L"warning: %s\n", msg);}
 inline void show_msg(const char* msg)  {if (Debug) fwprintf(stdout,L"Messenger: %s\n", msg);}

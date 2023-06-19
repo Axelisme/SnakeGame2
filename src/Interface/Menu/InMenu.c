bf3823a38494e6f3a7c00b6cea6ab9f9b7018db8
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2023-06-19 10:53:48 +0800

    ADD: InMenu breathe

--- src/Interface/Menu/InMenu.c
+++ src/Interface/Menu/InMenu.c
@@ -4,6 +4,10 @@
 const char IN_MENU_IMAGE_PATH[] = "data/image/menu/In_menu.png";
 //const char IN_MENU_IMAGE_PATH[] = "data/image/test2.png";
 
+static const int running_time_light_min = 150;
+static const int running_time_light_step = 1;
+static int direction = -1;
+
 InMenu* new_InMenu() {
     InMenu* self = new_SingleMenu(IN_MENU_IMAGE_PATH);
     InMenu_init(self);
@@ -20,6 +24,21 @@ void InMenu_init(InMenu* self) {
     Iself->event_dealer = _InMenu_deal_event;
 }
 void _InMenu_deal_event(Interface* Iself) {
+    // breathe light
+    if (direction < 0) {
+        Iself->background_light -= running_time_light_step;
+        if (Iself->background_light < running_time_light_min) {
+            Iself->background_light = running_time_light_min;
+            direction = 1;
+        }
+    } else {
+        Iself->background_light += running_time_light_step;
+        if (Iself->background_light > MAX_LIGHT) {
+            Iself->background_light = MAX_LIGHT;
+            direction = -1;
+        }
+    }
+
     if (Iself->event.type != ALLEGRO_EVENT_KEY_DOWN) return;
     Interface_set_kill(Iself, INTERFACE_START_MENU);
     Iself->event.type = NO_EVENT;
